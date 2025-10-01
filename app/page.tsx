50d78f856f6d0335ded4effe300d2b6ae0f55772
galih792 <akunmlbotke2@gmail.com>
2025-10-01 19:41:33 +0700

    edit gambar dan teks

--- app/page.tsx
+++ app/page.tsx
@@ -6,50 +6,77 @@ export default function Home() {
           🚘 Galeri Mobil Galih 
         </h1>
         <p className="text-gray-300 text-center sm:text-left max-w-2xl leading-relaxed">
-          Selamat datang di <span className="font-semibold text-red-400">galeri mobil Galih</span>.  
-          Pilih mobil impianmu dengan <span className="text-yellow-400">gaya</span>, kecepatan, dan kenyamanan terbaik.
+          Halo nama saya <span className="font-semibold text-red-400">Galih Ardy Pratama</span>.  
+          Ini <span className="text-yellow-400">webste</span> sederhana yang saya edit sendiri.
         </p>
 
-        {/* Mobil Showcase */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10 mt-6">
-          {/* Mobil 1 */}
+
           <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-red-500/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
-             <img
-              src="https://tse2.mm.bing.net/th/id/OIP.unMhzS3oo2NpAVmtjfFpEwHaE3?pid=Api&P=0&h=220"
+            <img
+              src="https://www.wallpaperbetter.com/wallpaper/823/337/175/mitsubishi-lancer-evolution-9-1080P-wallpaper.jpg"
               alt="evolution 9"
-              className="rounded-lg object-cover w-[400px] h-[250px]"
+              className="rounded-lg object-cover w-[600px] h-[210px]"
             />
             <h2 className="mt-4 text-xl font-bold text-white">Evolution 9</h2>
             <p className="text-gray-400 text-sm">⚡ Kecepatan tinggi dengan desain elegan.</p>
           </div>
 
-          {/* Mobil 2 */}
           <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-yellow-400/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
             <img
-              src="https://tse4.mm.bing.net/th/id/OIP.Vv4dAdepNzWEcAt8J787hwHaEF?pid=Api&P=0&h=220"
+              src="https://i.pinimg.com/originals/55/3b/08/553b08ac7cf2f0c1d6488881f2d90424.jpg"
               alt="city z"
-              className="rounded-lg object-cover w-[400px] h-[250px]"
+              className="rounded-lg object-cover w-[400px] h-[300px]"
             />
             <h2 className="mt-4 text-xl font-bold text-white">City Z</h2>
             <p className="text-gray-400 text-sm">✨ Nyaman, stylish, dan cocok buat nongki.</p>
           </div>
 
-          {/* Mobil 3 */}
           <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-blue-400/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
             <img
               src="https://nmaa.co.id/wp-content/uploads/2022/01/Honda-Accord-Ferio-1997-Reinaldi-BDG-4-e1642911829158.jpg"
               alt="civic ferio"
-              className="rounded-lg object-cover w-[400px] h-[250px]"
+              className="rounded-lg object-cover w-[600px] h-[210px]"
             />
             <h2 className="mt-4 text-xl font-bold text-white">Civic Ferio</h2>
             <p className="text-gray-400 text-sm">🔥 Ikonik, nyaman, dan kesukaan ciwi".</p>
+          </div>
+
+          <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-blue-400/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
+            <img
+              src="https://bringatrailer.com/wp-content/uploads/2022/07/1997_toyota_land-cruiser-fzj80_landcruiser-9-33887.jpg"
+              alt="VX 80"
+              className="rounded-lg object-cover w-[300px] h-[220px]"
+            />
+            <h2 className="mt-4 text-xl font-bold text-white">VX 80</h2>
+            <p className="text-gray-400 text-sm">🔥 Idaman bapak", auto dilirik calon mertua.</p>
+          </div>
+
+          <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-red-500/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
+            <img
+              src="https://img-ik.cars.co.za/images/2019/3/Suzuki%20Ertiga/tr:n-news_1200x/Suzuki_Ertiga-117.jpg"
+              alt="All New Ertiga Hybird"
+              className="rounded-lg object-cover w-[370px] h-[210px]"
+            />
+            <h2 className="mt-4 text-xl font-bold text-white">All New Ertiga Hybird</h2>
+            <p className="text-gray-400 text-sm">⚡ Salah satu mobil mpv keluarga paling nyaman.</p>
+          </div>
+
+          <div className="bg-gray-900 rounded-2xl shadow-lg hover:shadow-yellow-400/40 transition duration-300 transform hover:-translate-y-2 p-6 flex flex-col items-center border border-gray-700">
+            <img
+              src="https://wallpaperbat.com/img/146634874-suzuki-jimny.jpg"
+              alt="Jimny JB74"
+              className="rounded-lg object-cover w-[350px] h-[240px]"
+            />
+            <h2 className="mt-4 text-xl font-bold text-white">Jimny JB74</h2>
+            <p className="text-gray-400 text-sm">✨ Cocok buat bahan trabas dan ganteng.</p>
           </div>
         </div>
       </main>
 
       <footer className="bg-gray-950 text-gray-400 py-4 flex justify-center border-t border-gray-700">
         <p className="text-sm">
-          © 2025 <span className="text-red-400 font-semibold">Toko Mobil Galih</span> - Semua hak dilindungi 🚘
+          © 2025 <span className="text-red-400 font-semibold">Galeri Mobil</span> - Semua hak dilindungi 🚘
         </p>
       </footer>
     </div>
